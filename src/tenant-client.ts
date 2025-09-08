c4e56ad8676bb51894da24f7904e795aa8ce8dca
Hassan Kanso <hassankanso1095@gmail.com>
2025-09-08 23:58:41 +0200

    fix tenant response type mismatch

--- src/tenant-client.ts
+++ src/tenant-client.ts
@@ -2,7 +2,7 @@ import { BaseClient } from './base-client';
 import { CreateTenantRequest, PaginationParams, PaginatedResponse, TenantResponse } from './types';
 
 export class TenantClient extends BaseClient {
-  async getTenants(pagination?: PaginationParams): Promise<PaginatedResponse<TenantResponse[]>> {
+  async getTenants(pagination?: PaginationParams): Promise<PaginatedResponse<TenantResponse>> {
     return this.request('tenants', {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' },
